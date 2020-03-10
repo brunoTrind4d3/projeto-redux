8358fa82798ee4db6c8cd1b5ca241e9291ab2e8c
Bruno Trindade <b.l.trindade@outlook.com>
2020-03-10 18:28:34 -0300

    Create shopping cart reducer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,80 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_zoom1.jpg?ts=1571758175&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_zoom1.jpg?ts=1571758175&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_zoom1.jpg?ts=1571758175&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_zoom1.jpg?ts=1571758175&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_zoom1.jpg?ts=1571758175&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
